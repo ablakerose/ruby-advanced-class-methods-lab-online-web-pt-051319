6af755d73b372be503f15ca12afe1aae9c776099
Blake Rose <bdecktor@gmail.com>
2019-06-09 20:10:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   end
 
   def self.find_or_create_by_name(find_this_song)
-    song_already_exists = self.all.detect{ |song_name| song_name.name == find_this_song}
+    song_already_exists = self.find_by_name
       if song_already_exists == nil
         song = self.new
         song.name = find_this_song
