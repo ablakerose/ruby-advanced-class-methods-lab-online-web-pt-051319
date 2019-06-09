65ace2252d9f6db6529a13ae36a27dce9e8c610a
Blake Rose <bdecktor@gmail.com>
2019-06-09 19:53:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   def self.find_or_create_by_name(find_this_song)
     c = self.all.detect do |song_name|
       if song_name.name == find_this_song
-      end 
+      end
       if song_already_created == nil
       else
         song = self.new
