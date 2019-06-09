a33e518380b550a6bf1c0316b3a04ae9c1fbbf1e
Blake Rose <bdecktor@gmail.com>
2019-06-09 20:32:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,11 @@ class Song
     #   end
 
     self.find_by_name(string_name_of_song) || self.create_by_name(string_name_of_song)
-
+      #In Ruby, every value is truey unless it is nil or false
+      #This is evaluating the first value first, so this method will either find and therefore return a song (a song is a truthy value) OR
+      # it will not find a song and return "nil" which is a falsey value. If it finds a song, it will return the song and not execute
+      #the second method. If it does not find the song and returns "nil" then it will execute the second half of the OR statement
+      # which will create the name of the song
   end
 
 end
