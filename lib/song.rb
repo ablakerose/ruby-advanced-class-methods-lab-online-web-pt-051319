b02fd4e7bf2d1dd20a1ddf7dbfdaedf3e2288cec
Blake Rose <bdecktor@gmail.com>
2019-06-09 19:08:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,8 +16,9 @@ class Song
     self.class.all << self
   end
 
-  def self.new_by_name(song)
+  def self.new_by_name(string_name_of_song)
     song = self.new
+    song.name = string_name_of_the_song)
     song
   end
 
