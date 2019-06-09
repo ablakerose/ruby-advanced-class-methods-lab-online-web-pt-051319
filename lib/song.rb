4408678bcd9c7d98c351858db66d8c609603511c
Blake Rose <bdecktor@gmail.com>
2019-06-09 20:20:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
     song = self.find_by_name(string_name_of_song)
       if song == nil
         self.create_by_name(string_name_of_song)
-        
+
       else
         song
       end
